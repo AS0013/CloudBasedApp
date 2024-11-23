6dc7fb14ce8da420c8a0153d7cdf6241752b381c
Toby <tobiasnordskov@gmail.com>
2024-11-23 11:42:21 +0100

    Implemented instance_box

--- CloudAssignmentPart1/cloudassignment_part1/src/cloudassignment_part1/app.py
+++ CloudAssignmentPart1/cloudassignment_part1/src/cloudassignment_part1/app.py
@@ -5,6 +5,7 @@ from toga.style.pack import COLUMN, ROW
 
 
 class HelloWorld(toga.App):
+
     
     def startup(self):
         main_box = toga.Box(style=Pack(direction=COLUMN))
@@ -34,6 +35,75 @@ class HelloWorld(toga.App):
         # TODO add Containers and Widgets to your all_instances_box --- ATTA
         instance_box = toga.Box(style=Pack(direction=COLUMN,flex=1))
         # TODO add Containers and Widgets to your instance_box --- TOBIAS
+
+        all_instances = toga.Label(
+            text = "Each instance will appear here"
+        )
+
+        role_items = list([' ', 'Doctor', 'Nurse', 'Patient'])
+        selected_role_item = 'Doctor'
+        events = [
+            {'id': 'Diagnose', 'label': 'Diagnose', 'role': 'Doctor'},
+            {'id': 'Operate', 'label': 'Operate', 'role': 'Doctor'},
+            {'id': 'Give treatment', 'label': 'Give treatment', 'role': 'Nurse'},
+            {'id': 'Take treatment', 'label': 'Take treatment', 'role': 'Patient'}]
+        
+        main1_box = toga.Box(style = Pack(direction = ROW))
+
+        role_box = toga.Box(style = Pack(direction = COLUMN,  flex = 1))
+        role_label = toga.Label(
+            text = "Current Role:",
+            style=Pack(padding=(0, 5)),
+        )
+        select_role = toga.Label(
+            text = "Select other role:",
+            style=Pack(padding=(0, 5)),
+        )
+        self.selections = toga.Selection(items = role_items, 
+                                    value = role_items[0], 
+                                    on_change = self.role_changed,
+                                    style=Pack(padding=(0, 5)),
+                                    )
+        
+        role_box.add(role_label)
+        role_box.add(select_role)
+        role_box.add(self.selections)
+        main1_box.add(role_box)
+    
+
+        
+        instance_box_inner = toga.Box(style = Pack(direction = COLUMN, flex = 1))
+        instance_label = toga.Label(
+            text = "Current Instance:",
+            style=Pack(padding=(0, 5)),
+        )
+
+        added = toga.Label(
+            text = "Not added yet!",
+            style=Pack(padding=(0, 5)),
+        )
+
+        instance_box_inner.add(instance_label)
+        instance_box_inner.add(added)
+        main1_box.add(instance_box_inner)
+        
+
+
+        scroll = toga.ScrollContainer(horizontal=False,style=Pack(flex=1))
+        event_box = toga.Box(style=Pack(direction=COLUMN, padding = 5))
+        for event in events:
+            event_button = toga.Button(text =  f"{event['label']} (role: {event['role']})",
+                                       id = event['id'],
+                                       style=Pack( color='green'),
+                                       on_press = self.execute_event,)
+            event_box.add(event_button)
+        scroll.content = event_box
+
+        instance_box.add(all_instances)
+        instance_box.add(main1_box)
+        instance_box.add(scroll)
+        
+
         logout_box = toga.Box(style=Pack(direction=COLUMN,flex=1))
         # TODO add Containers and Widgets to your logout_box --- CAROLINE
 
@@ -51,6 +121,13 @@ class HelloWorld(toga.App):
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = option_container
         self.main_window.show()
+
+
+    def role_changed(self,widget):
+        print(f'[i] You changed the role to {widget.value}!')
+
+    def execute_event(self, widget):
+        print (f'[i] You want to execute event: {widget.id}')
         
     async def option_item_changed(self,widget):
         print('[i] You have selected another Option Item!')
